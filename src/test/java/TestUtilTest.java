5a30ce8f78585ea827cbb643a15940133198ae20
chacha86 <taejincha0619@gmail.com>
2026-02-06 10:37:10 +0900

    6 - r

--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,6 +24,19 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("현재를 사랑하라.");
         assertThat(author).isEqualTo("작자미상");
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutByteArray()")
+    void t2() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutByteArray();
+
+        System.out.println("안녕하세요");
+
+        String rst = byteArrayOutputStream.toString();
+
+        assertThat(rst).isEqualTo("안녕하세요");
+
     }
 
 //    @Test
